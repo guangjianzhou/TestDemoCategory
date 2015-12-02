894ceb9ef4ecb6394b8bffe3853dc7ba1b129f2e
guangjianzhou <ZGJ@ewit.local>
2015-12-02 13:08:15 +0800

    Initial Commit

---
+++ TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by guangjianzhou on 15/12/2.
+//  Copyright © 2015年 guangjianzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by guangjianzhou on 15/12/2.
+//  Copyright © 2015年 guangjianzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
